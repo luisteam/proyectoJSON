d56d03bcffd8d7186788c0405e782d63c65558b0
Luis Raya <luis_rayita@hotmail.es>
2018-03-09 00:53:41 +0100

    arreglado

--- funcion2.py
+++ funcion2.py
@@ -5,56 +5,20 @@ from pprint import pprint
 with open('300107-0-agenda-actividades-eventos.json') as data_file:    
  data = json.load(data_file)
 
-
- #pprint(data["@graph"]["excluded-days"])
-
-
 niños=[]
 mayores=[]
 
-listemp=[]
-
-
-
-
 for i in range(len(data["@graph"])):
-	titulo=(data["@graph"][i]["title"])
-	listemp.append(titulo)
-	audiencia=(data["@graph"][i]["excluded-days"])
-	pprint(audiencia)
+	objeto=(data["@graph"][i])
 
+	if 'audience' in objeto:
+		audiencia=(objeto["audience"])
+		titulo=(data["@graph"][i]["title"])
 
+		if "Niños" in audiencia:
+			niños.append(titulo)
 
+		if "Mayores" in audiencia:
+			mayores.append(titulo)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#	if audiencia == "Niños":
-#		listado.append(niños)
-#		listemp=[]
-#	if audiencia == "Mayores":
-#		listado.append(mayores)
-#		listemp=[]
-#	
-#
-#
-#
-#print(len(niños),len(mayores))
+print("Numero de eventos para: Niños: %d, Mayores: %d." % (len(niños),len(mayores)))
